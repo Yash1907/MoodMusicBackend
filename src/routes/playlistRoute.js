dcb11edc5b1be00fcf44022540d49d7022683002
[Yash1907] <[yash.mailacc@gmail.com]>
2024-11-23 16:36:10 -0500

    update playlist route

--- src/routes/playlistRoute.js
+++ src/routes/playlistRoute.js
@@ -34,8 +34,30 @@ playlistRouter.post("/create-playlist", authMiddleware, async (req, res) => {
   }
 });
 
-playlistRouter.get("/update-playlist", async(req, res) => {
+playlistRouter.put("/update-playlist/:id", authMiddleware, async(req, res) => {
+  const playlist_id = req.params.id;
+  const { name, coverImage } = req.body;
 
+  if(!name && !coverImage){
+    return res.status(400).json({ message: "At least one of the name or cover image is required to update the playlist" });
+  }
+  try{
+    const userId = req.user.id;
+    const {data: playlist, error: error} = await supabase.from("playlists").select("*").eq("playlist_id", playlist_id).eq("user_id", user_id).single();
+    if(error) return res.status(500).json({ message: "Error fetching playlists", error:error });
+    if(!playlist) return res.status(404).json({message: "Playlist not found"});
+    
+    const updateData = {};
+    if(name) updateData.name = name;
+    if(coverImage) updateData.cover_image = coverImage;
+
+    const { data: updatedPlaylist, error: updateError } = await supabase.from("playlists").update(updateData).eq("playlist_id", playlistId).eq("user_id", userId);
+    if(updateError) return res.status(500).json({ message: "Error updating playlist", error: updateError});
+    return res.status(200).json({ message: "Playlist updated successfully", playlist: updatedPlaylist[0] });
+  }
+  catch(err){
+    return res.send(500).json({ message: "Server error", error:err.message });
+  }
 });
 
 export { playlistRouter };
