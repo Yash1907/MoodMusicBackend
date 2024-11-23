78e4606d620de60838322464363346821a4f477f
[Yash1907] <[yash.mailacc@gmail.com]>
2024-11-23 16:38:33 -0500

    update playlist route

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-SUPABASE_URL=process.env.SUPABASE_URL
-SUPABASE_SERVICE_KEY=process.env.SUPABASE_SERVICE_KEY
-SUPABASE_JWT_SECRET=process.env.SUPABASE_JWT_SECRET
+// SUPABASE_URL=process.env.SUPABASE_URL
+// SUPABASE_SERVICE_KEY=process.env.SUPABASE_SERVICE_KEY
+// SUPABASE_JWT_SECRET=process.env.SUPABASE_JWT_SECRET
+SUPABASE_URL="https://irhaodqloxfcxhfwwula.supabase.co/"
+SUPABASE_SERVICE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyaGFvZHFsb3hmY3hoZnd3dWxhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTU0MDA2NCwiZXhwIjoyMDQ1MTE2MDY0fQ.DNQDLZ768r7bZExL9m0PKt6O7k9O0x1f_49moENBuoM"
+SUPABASE_JWT_SECRET="CKdyIaB+Q7D8yBcutYPKhHsB5J4tA0ugBEArdebkNN27pOKNWZir7XGuLarCoyJ+KrILtyTQUPzC4cd7DRO8YA=="
 
 /**
  * @type {import('@supabase/supabase-js').SupabaseClient}
