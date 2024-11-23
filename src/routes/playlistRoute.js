765f079706823a69a84a947f41db1a6c09dc5706
[Yash1907] <[yash.mailacc@gmail.com]>
2024-11-23 16:52:29 -0500

    delete playlist route

--- src/routes/playlistRoute.js
+++ src/routes/playlistRoute.js
@@ -60,4 +60,19 @@ playlistRouter.put("/update-playlist/:id", authMiddleware, async(req, res) => {
   }
 });
 
+playlistRouter.delete("/delete-playlist/:id", authMiddleware, async(req, res) => {
+  const playlistId = req.params.id;
+  try{
+    const userId = req.user.id;
+    const { data: playlist, error: error } = await supabase.from("playlists").select("*").eq("playlist_id",playlistId).eq("user_id",userId).single();
+    if(error) return res.status(500).json({ message:"Error fetching playlist",error:error });
+    if(!playlist) return res.status(404).json({ message:"Playlist not found" });
+    const { error: deleteError } = await supabase.from("playlists").delete().eq("playlist_id",playlistId).eq("user_id",userId);
+    if(deleteError) return res.send(500).json({ message:"Error deleting playlist", error:deleteError });
+    return res.status(200).json({ message:"Playlist deleted successfully" });
+  } catch(err){
+    return res.status(500).json({ message:"Server error", error:err.message });
+  }
+});
+
 export { playlistRouter };
